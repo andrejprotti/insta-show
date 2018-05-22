3b348ddd29e5013d0bfd2115a0df6056398d263c
André Protti <andrejardimprotti@gmail.com>
2018-05-21 21:55:12 -0300

    Creates proper view for pictures#show

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,8 @@
 class PicturesController < ApplicationController
 
   def show
-    client = Instagram.client(:access_token => session[:access_token])
-    user = client.user
-    html = "<h1> #{user.username} pictures:</h1>"
-    for media_item in client.user_recent_media
-      html << "<div style='float:left;'><img src='#{media_item.images.thumbnail.url}'><br/></div>"
-    end
-    render inline: html
+    @client = Instagram.client(:access_token => session[:access_token])
+    @user = @client.user
   end
 
 end
